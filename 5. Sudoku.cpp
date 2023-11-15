2275e2049993b8deaba74ee70bfc3ede910f64ae
Rubayet Sadman Sami <69534625+Sami-63@users.noreply.github.com>
2023-11-15 03:34:05 +0000

    Update 5. Sudoku.cpp

--- 5. Sudoku.cpp
+++ 5. Sudoku.cpp
@@ -5,9 +5,6 @@ using namespace std;
 
 const int N = 9;
 
-// GPT
-
-// Function to print the Sudoku grid
 void printSudoku(const array<array<int, N>, N>& grid) {
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
@@ -17,16 +14,13 @@ void printSudoku(const array<array<int, N>, N>& grid) {
     }
 }
 
-// Function to check if a number can be placed at a given position
 bool isSafe(const array<array<int, N>, N>& grid, int row, int col, int num) {
-    // Check if 'num' is not present in the current row and column
     for (int x = 0; x < N; ++x) {
         if (grid[row][x] == num || grid[x][col] == num) {
             return false;
         }
     }
 
-    // Check if 'num' is not present in the current 3x3 subgrid
     int startRow = row - row % 3;
     int startCol = col - col % 3;
     for (int i = 0; i < 3; ++i) {
@@ -40,11 +34,10 @@ bool isSafe(const array<array<int, N>, N>& grid, int row, int col, int num) {
     return true;
 }
 
-// Function to solve Sudoku using backtracking
+
 bool solveSudoku(array<array<int, N>, N>& grid) {
     int row, col;
 
-    // Find an empty position in the grid
     bool isEmpty = false;
     for (row = 0; row < N; ++row) {
         for (col = 0; col < N; ++col) {
@@ -58,32 +51,27 @@ bool solveSudoku(array<array<int, N>, N>& grid) {
         }
     }
 
-    // If there is no empty position, the Sudoku is solved
+
     if (!isEmpty) {
         return true;
     }
 
-    // Try placing a number from 1 to 9 at the current position
     for (int num = 1; num <= 9; ++num) {
         if (isSafe(grid, row, col, num)) {
             grid[row][col] = num;
 
-            // Recursively try to solve the remaining Sudoku
             if (solveSudoku(grid)) {
                 return true;
             }
 
-            // If placing 'num' does not lead to a solution, backtrack
             grid[row][col] = 0;
         }
     }
 
-    // If no number can be placed at the current position, backtrack
     return false;
 }
 
 int main() {
-    // Example Sudoku grid (0 represents empty cells)
     array<array<int, N>, N> sudokuGrid = {
         {5, 3, 0, 0, 7, 0, 0, 0, 0},
         {6, 0, 0, 1, 9, 5, 0, 0, 0},
